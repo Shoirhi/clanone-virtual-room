e475fa4206be9146c31f29ba088e5dcf867e6e72
Shoirhi <dibella.flqxl@simplelogin.com>
2024-06-22 18:39:19 +0900

    環境を修正

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -5,37 +5,66 @@ import {
   OrbitControls,
   PointerLockControls,
   FlyControls,
-  Sky
+  Sky,
 } from "@react-three/drei";
 import { Button } from "./ui/button";
+import {
+  Bloom,
+  EffectComposer,
+  Noise,
+  Vignette,
+  SSAO, DepthOfField, ToneMapping
+} from "@react-three/postprocessing";
 
 import Model from "./Room";
 import { useState, useEffect } from "react";
+import { Player } from "./Player";
 
 export default function Experience() {
-  const [showInstructions, setShowInstructions] = useState(true)
+  const [showInstructions, setShowInstructions] = useState(true);
 
   function pointerLockChange() {
-    setShowInstructions(!showInstructions)
+    setShowInstructions(!showInstructions);
   }
 
   useEffect(() => {
-    document.addEventListener("pointerlockchange", pointerLockChange, false)
+    document.addEventListener("pointerlockchange", pointerLockChange, false);
     return () => {
-      document.removeEventListener("pointerlockchange", pointerLockChange, false)
-    }
-  })
+      document.removeEventListener(
+        "pointerlockchange",
+        pointerLockChange,
+        false
+      );
+    };
+  });
   return (
     <>
-      <Canvas camera={{position: [0, 4, 0]}} shadows>
+      <Canvas camera={{ position: [0, 4, 0] }} shadows>
         <PointerLockControls makeDefault selector="#button" />
         <FlyControls />
         <ambientLight intensity={2} />
-        <Sky distance={450000} sunPosition={[0, 1, 0]} inclination={0} azimuth={0.25} />
-        <Model castShadow receiveShadow />
+        <Sky
+          distance={450000}
+          sunPosition={[0, 1, 0]}
+          inclination={0}
+          azimuth={0.25}
+        />
+        <Model receiveShadow />
+        <EffectComposer>
+          <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
+          <Noise opacity={0.02} />
+          <Vignette eskil={false} offset={0.02} darkness={0.5} />
+        </EffectComposer>
       </Canvas>
-      <div className={cn("absolute inset-0 flex justify-center items-center", showInstructions ? "absolute" : "hidden")}>
-        <Button id="button" size="lg" className="absolute">バーチャル内見に入る</Button>
+      <div
+        className={cn(
+          "absolute inset-0 flex justify-center items-center",
+          showInstructions ? "absolute" : "hidden"
+        )}
+      >
+        <Button id="button" size="lg" className="absolute">
+          バーチャル内見に入る
+        </Button>
       </div>
     </>
   );
