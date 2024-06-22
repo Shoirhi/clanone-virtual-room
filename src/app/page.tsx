dbb540d3d31c5f75aa6193355eaaf61be3cc344b
Shoirhi <dibella.flqxl@simplelogin.com>
2024-06-22 14:03:00 +0900

    chore: Update npm dependencies and add runtime configuration
    - Update npm dependency "@cloudflare/next-on-pages" to version "^1.11.3"
    - Add runtime configuration for "edge" in "page.tsx" and "layout.tsx"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Experience from "@/components/Experience";
 
+export const runtime = 'edge';
+
 export default function Home() {
   return (
     <div className="relative w-full h-dvh">
